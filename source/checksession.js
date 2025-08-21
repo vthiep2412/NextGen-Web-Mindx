7abc178f1e82927742ccda15cb662a861a5528c1
vthiep2412 <vthiep2412@gmail.com>
2025-08-21 20:54:34 +0700

    dell

--- source/checksession.js
+++ source/checksession.js
@@ -5,13 +5,13 @@ function checkSession() {
         if (now > userSession.expiry) {
             // Phiên đã hết hạn, xóa dữ liệu và chuyển hướng về trang đăng nhập
             localStorage.removeItem('user_session');
-            if(window.location.href != "index.html"){
-                window.location.href = "index.html";
+            if(!location.pathname.endsWith('index.html')){
+                location.href = location.pathname.replace(/[^\/]+$/, 'index.html');
             }
         } else { 
             console.log("Phiên còn hợp lệ");
-            if(window.location.href == "index.html"){
-                window.location.href = 'main.html';
+            if(window.location.pathname.endsWith("index.html")){
+                location.href = location.pathname.replace(/[^\/]+$/, 'main.html');
             }
             syncFirebaseCartToLocal();
             syncLocalCartToFirebase();
@@ -19,8 +19,8 @@ function checkSession() {
     } catch (error){
         // Không có phiên, chuyển hướng về trang đăng nhập
         console.error(`Không có phiên: ${error}`);
-        if(window.location.href != "index.html"){
-            window.location.href = "index.html";
+        if(!location.pathname.endsWith('index.html')){
+            location.href = location.pathname.replace(/[^\/]+$/, 'index.html');
         }
     }
 }
